98a427f2f24d34859ce9963286cdc628173deacb
AlzyWelzy <welzyalzy@gmail.com>
2022-12-25 19:58:36 +0530

    worked

--- 12Projects/guessNumber/main-v2.py
+++ 12Projects/guessNumber/main-v2.py
@@ -14,12 +14,12 @@ class Game:
         self.min_num = min_num
         self.max_num = max_num
         self.target_num = None
+        self.create_ui()
         self.start_game()
 
     def start_game(self):
         self.target_num = random.randint(self.min_num, self.max_num)
         self.player.guesses = 0
-        self.guess()
 
     def guess(self):
         try:
