dda8c1a81834080e0aa1931e7025b9a54c11bdcb
AlzyWelzy <welzyalzy@gmail.com>
2022-12-26 16:10:47 +0530

    added another functionality

--- 12Projects/rock_paper_scissor/main.py
+++ 12Projects/rock_paper_scissor/main.py
@@ -9,6 +9,7 @@ class RockPaperScissor:
             "paper": "rock",
             "scissor": "rock"
         }
+        self.result = {"win": 0, "lose": 0, "tie": 0}
 
     def game(self):
 
@@ -23,14 +24,18 @@ class RockPaperScissor:
             print(f"The computer chose {computer_choice}.")
 
             if player_choice == computer_choice:
+                self.result["tie"] += 1
                 print("It's a tie!")
             elif self.rules[player_choice] == self.rules[computer_choice]:
+                self.result["win"] += 1
                 print("You win!")
             else:
+                self.result["lose"] += 1
                 print("You lose!")
 
             player_again = input("Do you want to play again? [y/n].")
             if player_again.lower() != "y":
+                print(f"Result is {self.result['win']} : {self.result['lose']} : {self.result['tie']}")
                 break
 
 
